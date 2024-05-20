2945fac3741750c13e3d382d7abf0b525383b7c2
ajichiatsuto <s2410004@jaist.ac.jp>
2024-05-20 17:09:39 +0900

    add cancer

--- classification_forge.py
+++ classification_forge.py
@@ -1,10 +1,20 @@
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import mglearn as mg
+import matplotlib.pyplot as plt
 
 X, y = mg.datasets.make_forge()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-clf = KNeighborsClassifier(n_neighbors=3)
-clf.fit(X_train, y_train)
-print("Test Set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X_train, y_train)
+    print('Test set accuracy: {:.2f}'.format(clf.score(X_test, y_test)))
+    mg.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+    mg.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title('{} neighbor(s)'.format(n_neighbors))
+    ax.set_xlabel('feature 0')
+    ax.set_ylabel('feature 1')
+
+plt.show()
